5997af25d79f633461b5df5e162d23884c2b8f8c
ebundala <ebundala@gmail.com>
2018-07-24 12:25:08 +0300

    added boileplate code for user model

---
+++ application/models/User_model.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+class User_model extends CI_model{
+
+ function login($email,$password){
+
+ }
+ function registerUser($username,$password,$email){
+
+ }
+
+ function logout(){}
+ function getUserPermissions(){}
+ function deleteUser(){}   
+ function getUsers(){}      
+
+
+
+}
+
+
+?>
